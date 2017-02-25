83d9cb55b40d502c640dd49ad55188e8ab3a7426
sasha42 <sasha42@users.noreply.github.com>
2017-02-25 18:07:55 +0100

    diabled debug and added public facing interface

--- server.py
+++ server.py
@@ -51,4 +51,4 @@ if __name__ == '__main__':
         pyproj.Proj(init='epsg:4326'),
         pyproj.Proj('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs'))
     
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
